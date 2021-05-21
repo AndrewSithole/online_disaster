9190ff0a9b5d151626d4b8656f39ac84d290733a
Andrew <andrews@actionstarter.co.uk>
2021-05-21 10:57:01 +0200

    Changed the database and classifier locations

--- app/run.py
+++ app/run.py
@@ -45,11 +45,11 @@ def tokenize(text):
 
 
 # load data
-engine = create_engine('sqlite:///data/messages.db')
+engine = create_engine('sqlite:///../data/messages.db')
 df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("models/classifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
